48dc6d151347e5ecc997298c38472c9fb09eb3c4
PrathamGhaywat <Pratham.Ghaywat@outlook.com>
2025-10-04 17:06:11 +0200

    ok so i created the api for cheking and made the prompt for the question better(the question is easier now). I don't want to torture the user!

---
+++ app/api/check/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const { question, answer } = await req.json()
+
+    const checkPrompt = `Question: ${question} \nAnswer: {answer} \nDecide if the answe is correct. Reply only "true" or false. ` //The prompt should have been to shut him down if gets it wrong but idk
+    const res = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${process.env.OPENROUTER_API_KEY}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            model: "mistralai/mistral-small-3.2-24b-instruct:free", //this model because it is really fast. and free lol
+            messages: [{ role: "system", content: checkPrompt}]
+        })
+    })
+
+    const data = await res.json();
+    const correct = /true/i.test(data.choices?.[0]?.message?.content);
+    return NextResponse.json({ correct })
+}
